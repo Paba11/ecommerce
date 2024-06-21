cdf4fbcac2878c4ff72bf568e4a271b6a615dd6c
PaoloSbarzagli <paolo.sbarzagli@gmail.com>
2024-06-21 09:59:46 +0200

    Aggiunta vista ordini precedenti

--- store/views.py
+++ store/views.py
@@ -78,7 +78,6 @@ def updateItem(request):
     return JsonResponse('Item was added', safe=False)
 
 
-@csrf_exempt
 def processOrder(request):
     transaction_id = datetime.datetime.now().timestamp()
     data = json.loads(request.body)
